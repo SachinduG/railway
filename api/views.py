8312f3e8c9ad2f4a1ff1f799700869ddcbafbbc0
Sachindu <sachindugimhana0@gmail.com>
2022-10-10 12:12:16 +0530

    4th commit

--- api/views.py
+++ api/views.py
@@ -15,8 +15,8 @@ def getPrecdict(request):
     data = request.data
     print(data)
     message = data['message']
-    output = get_response(data)
-    sa = get_sa(data)
+    output = get_response(message)
+    sa = get_sa(message)
     result = output+","+str(sa)
     print(result)
     return Response(result)
