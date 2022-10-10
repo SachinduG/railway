7fe5493a6e8dcd7de1cf3f108c795caa2fbf5d42
Sachindu <sachindugimhana0@gmail.com>
2022-10-10 15:35:53 +0530

    4th commit

--- api/views.py
+++ api/views.py
@@ -12,7 +12,7 @@ def getChat():
 @api_view(['POST'])
 def getPredict(request):
     data = request.data
-    print(data.dict())
+    print(data.dict().get('_contents'))
     if 'message' in request.data:
         message = request.data['message']
     else:
