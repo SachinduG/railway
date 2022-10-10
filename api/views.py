3589fd8e9508d495295c9679781e6c390a06e88e
Sachindu <sachindugimhana0@gmail.com>
2022-10-10 13:26:07 +0530

    4th commit

--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ def getChat():
 def getPredict(request):
     data = request.data
     print(data)
-    message = request.POST.get('message', '')
+    message = request.data.get('message', None)
     output = get_response(message)
     sa = get_sa(message)
     result = output+", "+str(sa)
